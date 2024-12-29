75feba25ba0db9ab74d0f89ae65c0fb07eae8399
BrunOnurBMB <bruno.silveira.0803@gmail.com>
2024-12-29 18:06:46 -0300

    adicionando pasta sobre arrays

---
+++ base/arrays/BelowAVG.java
@@ -0,0 +1,47 @@
+package Vet;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class BelowAVG {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int N;
+		double avg;
+		avg = 0;
+		
+		System.out.println("How many elemnts does the array have?");
+		N = sc.nextInt();
+		sc.nextLine();
+		System.out.println("-------------");
+		
+		double vet[] = new double[N];
+		double vetBelow[] = new double[N];
+		
+		for (int i = 0; i < N; i++) {
+			System.out.println("Add a number:");
+			vet[i] = sc.nextDouble();
+		}
+		
+		for (int i = 0; i < vet.length; i++) {
+			avg = avg + vet[i];
+		}
+		avg = avg / N;
+		
+		System.out.println("-------------");
+		System.out.println("Average of the array = " + String.format("%.3f", avg));
+		System.out.println("Elements below the average:");
+		for (int i = 0; i < N; i++) {
+			if (vet[i] < avg) {
+				vetBelow[i] = vet[i];
+				System.out.println(String.format("%.1f", vetBelow[i]));
+			}
+			
+		}
+		sc.close();	
+	}
+
+}
---
+++ base/arrays/Height.java
@@ -0,0 +1,68 @@
+package Vet;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Height {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int N, sum;
+		double AVG;
+		AVG = 0;
+		sum = 0;
+		
+		System.out.println("Insert the number of people who are included:");
+		N = sc.nextInt();
+		sc.nextLine();
+		
+		double[] vetHeight = new double[N];
+		int[] vetAge = new int [N];
+		String[] vetName = new String[N];
+		
+		System.out.println("Insert the name, age, and height of each person.");
+		System.out.println();
+		for (int i = 0; i < N; i++) {
+			System.out.println("** New person:"
+					+ " **");
+			
+			System.out.println("Name:");
+			vetName[i] = sc.nextLine();
+			
+			System.out.println("Age:");
+			vetAge[i] = sc.nextInt();
+			
+			System.out.println("Height:");
+			vetHeight[i] = sc.nextDouble();
+			
+			sc.nextLine();
+			
+			System.out.println();
+		}
+		
+		for (int i = 0; i < vetHeight.length; i++) {
+			AVG += vetHeight[i];
+		}
+		System.out.println("Average height: " + String.format("%.2f", (AVG / N)));
+		
+		for (int i = 0; i < vetAge.length; i++) {
+			
+			if(vetAge[i] < 16) {
+				sum++;
+			}	
+		}
+		System.out.println("Person with less 16 years: " + (sum * 100)/N+"%");
+		
+		for (int i = 0; i < N; i++) {
+			
+			if(vetAge[i] < 16) {
+				System.out.println(vetName[i]);
+			}	
+		}
+		sc.close();
+
+	}
+
+}
